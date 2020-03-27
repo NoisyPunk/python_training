90ae517638d47a4986a7f9bea62740aeee2ac274
solodovnikov <solodovnikov@electronika.ru>
2020-03-27 10:13:07 +0300

    Исключен драйвер из тестов и перенесен внутрь методов, + устранена передача драйвера в качестве параметра

--- test_add_group.py
+++ test_add_group.py
@@ -14,10 +14,12 @@ class TestAddGroup(unittest.TestCase):
 
     def open_home_page(self, wd):
         # open homepage
+        wd = self.wd
         wd.get("http://localhost/addressbook/group.php")
 
-    def login(self, wd, username, password):
+    def login(self, username, password):
         # login
+        wd = self.wd
         self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
@@ -28,13 +30,15 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
-    def open_group_page(self, wd):
+    def open_group_page(self):
         # Open group page
+        wd = self.wd
         wd.find_element_by_link_text("groups").click()
 
-    def create_group(self, wd, group):
+    def create_group(self, group):
         # Create group
-        self.open_group_page(wd)
+        wd = self.wd
+        self.open_group_page()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -45,33 +49,34 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        self.submit_group_create(wd)
-        self.return_group_page(wd)
+        self.submit_group_create()
+        self.return_group_page()
 
-    def submit_group_create(self, wd):
+    def submit_group_create(self):
         # submit group creation
+        wd = self.wd
         wd.find_element_by_name("submit").click()
 
-    def return_group_page(self, wd):
+    def return_group_page(self):
         # return group page
+        wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self, wd):
+    def logout(self):
         # logout
+        wd = self.wd
         wd.find_element_by_link_text("Logout").click()
 
     def test_add_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="dfre", header="dfre", footer='123'))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="dfre", header="dfre", footer='123'))
+        self.logout()
 
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.create_group(wd, Group(name="", header="", footer=''))
-        self.logout(wd)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=''))
+        self.logout()
 
     def tearDown(self):
         self.wd.quit()
