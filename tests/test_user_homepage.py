ed2a39a11ea66f5125321f700026268e955b9c8e
NoisyPunk <inismonabs@yandex.ru>
2020-04-03 00:14:31 +0300

    Добавил тест удаления юзера + тест на дом страницу юзера

--- tests/test_user_homepage.py
+++ tests/test_user_homepage.py
@@ -1,6 +1,6 @@
 from model.user import User
 
-def test_del_user(app):
-    app.session.login(username="admin", password="secret")
-    app.user.delete_first_user()
-    app.session.logout()
+# def test_del_user(app):
+#     app.session.login(username="admin", password="secret")
+#     app.user.delete_first_user()
+#     app.session.logout()
