bf1fe608c1743705e43e524ceafa16064cc061fa
NoisyPunk <inismonabs@yandex.ru>
2020-04-05 14:40:05 +0300

    Рефакторинг модификации и заполнения групп, переименование метода модификацйии

--- fixture/group.py
+++ fixture/group.py
@@ -12,6 +12,12 @@ class groupHelper:
         # Create group
         wd = self.app.wd
         self.open_group_page()
+        self.fill_group_form(group)
+        self.submit_group_create()
+        self.return_group_page()
+
+    def fill_group_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -22,8 +28,6 @@ class groupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        self.submit_group_create()
-        self.return_group_page()
 
     def submit_group_create(self):
         # submit group creation
@@ -33,19 +37,33 @@ class groupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_group_page()
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         wd.find_element_by_name("delete").click()
         self.return_group_page()
 
-    def edit_first_group(self, group):
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_group_page()
-        wd.find_element_by_name("selected[]").click()
-        wd.find_element_by_xpath("/html/body/div/div[4]/form/input[3]").click()
-        wd.find_element_by_xpath("/html/body/div/div[4]/form/input[2]").clear()
-        wd.find_element_by_xpath("/html/body/div/div[4]/form/input[2]").send_keys(group.name)
-        wd.find_element_by_xpath("/html/body/div/div[4]/form/input[3]").click()
-        wd.find_element_by_xpath("/html/body/div/div[4]/div/i/a").click()
+        self.select_first_group()
+        # open modif group
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(new_group_data)
+        # submit modify
+        wd.find_element_by_name("Update").click()
+
+        self.return_group_page()
+
+
+        # wd.find_element_by_xpath("/html/body/div/div[4]/form/input[3]").click()
+        # wd.find_element_by_xpath("/html/body/div/div[4]/form/input[2]").clear()
+        # wd.find_element_by_xpath("/html/body/div/div[4]/form/input[2]").send_keys(group.name)
+        # wd.find_element_by_xpath("/html/body/div/div[4]/form/input[3]").click()
+        # wd.find_element_by_xpath("/html/body/div/div[4]/div/i/a").click()
 
 
     def return_group_page(self):
