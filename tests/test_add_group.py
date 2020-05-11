1afe6b88b3d6ae33480619bbd4fb0bf42805bd24
NoisyPunk <inismonabs@yandex.ru>
2020-05-11 17:17:09 +0300

    Добавлен первичный метод прогона теста на разных данных

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -4,13 +4,20 @@ from sys import maxsize
 
 
 def test_add_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="dfre", header="dfre", footer='123')
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    test_data = [
+        Group(name="dfre", header="dfre", footer='123'),
+        Group(name="", header="", footer='')
+
+    ]
+    for group in test_data
+        pass
+        # old_groups = app.group.get_group_list()
+        # group = Group(name="dfre", header="dfre", footer='123')
+        # app.group.create(group)
+        # assert len(old_groups) + 1 == app.group.count()
+        # new_groups = app.group.get_group_list()
+        # old_groups.append(group)
+        # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 # def test_add_empty_group(app):
 #     old_groups = app.group.get_group_list()
